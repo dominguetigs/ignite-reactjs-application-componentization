37ed96167c6d862653335ba9035cb56dc65b4300
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-06-13 17:43:06 -0300

    feat: separate main content in a component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,17 @@
-import { useEffect, useState } from 'react';
-
-import { MovieCard } from './components/MovieCard';
+import { useState } from 'react';
 
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
-
-import { api } from './services/api';
+import { Content } from './components/Content';
 
 import './styles/global.scss';
-
-import './styles/content.scss';
-
-import { GenreResponseProps } from './interfaces/GenreResponseProps';
-import { MovieProps } from './interfaces/MovieProps';
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then((response) => {
-      setMovies(response.data);
-    });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then((response) => {
-      setSelectedGenre(response.data);
-    });
-  }, [selectedGenreId]);
-
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar handleMenuClick={setSelectedGenreId} />
-
-      <div className="container">
-        <header>
-          <span className="category">
-            Categoria:<span> {selectedGenre.title}</span>
-          </span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map((movie) => (
-              <MovieCard
-                key={movie.imdbID}
-                title={movie.Title}
-                poster={movie.Poster}
-                runtime={movie.Runtime}
-                rating={movie.Ratings[0].Value}
-              />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content selectedGenreId={selectedGenreId} />
     </div>
   );
 }
